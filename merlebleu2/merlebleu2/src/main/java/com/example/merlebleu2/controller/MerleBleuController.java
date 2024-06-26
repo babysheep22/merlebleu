d47b2ad546c227c0a94b8403acfba78858822b74
babysheep22 <90301724+babysheep22@users.noreply.github.com>
2024-06-26 17:52:37 +0900

    dd

--- merlebleu2/merlebleu2/src/main/java/com/example/merlebleu2/controller/MerleBleuController.java
+++ merlebleu2/merlebleu2/src/main/java/com/example/merlebleu2/controller/MerleBleuController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/merlebleu/")
 public class MerleBleuController {
 
 
